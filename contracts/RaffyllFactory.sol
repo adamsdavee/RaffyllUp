ea2c18d0573baef9e906ef3f8cb6e2883f1bbc58
adamsdave <davidkenechukwu591@gmail.com>
2025-10-18 01:52:59 +0100

    new-contract: RafyllFactory

--- contracts/RaffyllFactory.sol
+++ contracts/RaffyllFactory.sol
@@ -1,28 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "./CommunityRaffyll.sol";
+import { CommunityRaffyll } from "./CommunityRaffyll.sol";
 
 
-// contract RaffylFactory {
+contract RaffylFactory {
 
-//     event CommunityCreated(address indexed admin, address communityAddress, string name, string ens);
+    event CommunityCreated(address indexed admin, address communityAddress, string name, string ens);
 
-//     address[] public communities;
-//     mapping(address => address[]) public adminToCommunities;
+    address[] public communities;
+    mapping(address => address[]) public adminToCommunities;
 
-//     function createCommunity(address admin, string calldata name, string calldata ens) external returns (address) {
+    function createCommunity(address admin, string calldata name, string calldata ens) external returns (address) {
 
-//         require(adminToCommunities[admin] == address[](0), "User has community");
+        require(adminToCommunities[admin].length == 0, "User has community");
 
-//         CommunityRaffyll community = new CommunityRaffyll(admin, name, ens, address(this));
-//         communities.push(address(community));
-//         adminToCommunities[admin].push(address(community));
-//         emit CommunityCreated(admin, address(community), name, ens);
-//         return address(community);
-//     }
+        CommunityRaffyll community = new CommunityRaffyll(admin, name, ens, address(this));
 
-//     function getCommunities() external view returns (address[] memory) {
-//         return communities;
-//     }
-// }
+        communities.push(address(community));
+        adminToCommunities[admin].push(address(community));
+        emit CommunityCreated(admin, address(community), name, ens);
+        return address(community);
+    }
+
+    function getCommunities() external view returns (address[] memory) {
+        return communities;
+    }
+}
